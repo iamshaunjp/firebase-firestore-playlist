e6d0dcb9f1ccdbf31b15489d06432cb6538fb06f
Shaun <shaunjp@shaun-pelling.com>
2018-05-24 17:43:26 +0100

    lesson 8

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ function renderCafe(doc){
 }
 
 // getting data
-db.collection('cafes').orderBy('city').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    });
-});
+// db.collection('cafes').orderBy('city').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     });
+// });
 
 // saving data
 form.addEventListener('submit', (e) => {
@@ -43,4 +43,18 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
+});
+
+// real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        } else if (change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    });
 });
