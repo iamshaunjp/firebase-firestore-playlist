20969276a490e4262210866a9daf82b8953f3080
Shaun <shaunjp@shaun-pelling.com>
2018-05-24 17:37:17 +0100

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function renderCafe(doc){
 }
 
 // getting data
-db.collection('cafes').orderBy('city').get().then(snapshot => {
+db.collection('cafes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });
