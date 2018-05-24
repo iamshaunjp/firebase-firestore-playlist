b0a4dbc67b04a1fc1d06ab18fa7a5b46217112e3
Shaun <shaunjp@shaun-pelling.com>
2018-05-24 17:19:24 +0100

    lesson 3

---
+++ app.js
@@ -0,0 +1,24 @@
+const cafeList = document.querySelector('#cafe-list');
+
+// create element & render cafe
+function renderCafe(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
+
+// getting data
+db.collection('cafes').orderBy('city').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc);
+    });
+});
