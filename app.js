407ec69b4e499c08697ba9c1c6f949e691b07619
Shaun <shaunjp@shaun-pelling.com>
2018-05-24 17:25:35 +0100

    lesson 4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element & render cafe
 function renderCafe(doc){
@@ -21,4 +22,15 @@ db.collection('cafes').orderBy('city').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });
+});
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
 });
