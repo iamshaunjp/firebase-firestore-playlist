548442671aa836ee847759660970f71e2380830e
Shaun <shaunjp@shaun-pelling.com>
2018-05-24 17:44:39 +0100

    lesson 9

--- app.js
+++ app.js
@@ -58,3 +58,17 @@ db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
         }
     });
 });
+
+// updating records (console demo)
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').update({
+//     name: 'mario world'
+// });
+
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').update({
+//     city: 'hong kong'
+// });
+
+// setting data
+// db.collection('cafes').doc('DOgwUvtEQbjZohQNIeMr').set({
+//     city: 'hong kong'
+// });
